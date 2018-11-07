d1cabdb58bec4b7bee1c3d825bb1820a00904c40
MateuszMarczuk <mateuszmarczuk95@gmail.com>
2018-11-07 11:53:54 +0100

    Initial commit

---
+++ src/main/java/com/example/tim/TimFinalllApplication.java
@@ -0,0 +1,12 @@
+package com.example.tim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimFinalllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimFinalllApplication.class, args);
+	}
+}
