621a3dd7991a3af59e95fbb7e05ad8a49dfcf81e
MateuszMarczuk <mateuszmarczuk95@gmail.com>
2018-11-08 08:47:02 +0100

    INIT COMMIT 2

---
+++ src/main/java/com/example/tim/Service/Service.java
@@ -0,0 +1,8 @@
+package com.example.tim.Service;
+
+/**
+ * Created by Mateusz Marczuk
+ * Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, Warszawa 08.11.2018.
+ */
+public class Service {
+}
