5ab73d5f47be25e9d7e9650d4d2f725f8a0afa26
przytula94 <przytula94@gmail.com>
2018-11-26 23:48:18 +0100

    Relacja User-Subject

--- src/main/java/com/example/tim/Model/User.java
+++ src/main/java/com/example/tim/Model/User.java
@@ -3,6 +3,8 @@ package com.example.tim.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -36,10 +38,20 @@ public class User {
             name = "role_id", referencedColumnName = "id")
 
     )
-
-
     private Collection<Role> roles;
 
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Fetch(value = FetchMode.SUBSELECT)
+    @JoinTable(
+            name = "user_subject",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "subject_id", referencedColumnName = "id")
+
+    )
+    private Collection<Subject> subjects;
 
 
 
